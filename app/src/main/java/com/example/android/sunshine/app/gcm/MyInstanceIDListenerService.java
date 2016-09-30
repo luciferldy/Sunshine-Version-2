5ed2829b415cf8609603adbf5791904472008a36
luciferldy <2460572865@qq.com>
2016-09-30 22:07:39 +0800

    registering the app with gcm, commented out the gcm sender id

---
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyInstanceIDListenerService.java
@@ -0,0 +1,26 @@
+package com.example.android.sunshine.app.gcm;
+
+import android.content.Intent;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by lian_ on 2016/9/30.
+ */
+
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String LOG_TAG = MyInstanceIDListenerService.class.getSimpleName();
+
+    /**
+     * Called if instanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. This call is initiated by the
+     * InstanceID provider.
+     */
+    @Override
+    public void onTokenRefresh() {
+        // Fetch updated Instance ID token.
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
+}
