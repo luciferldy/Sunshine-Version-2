a20dca01f56d77956f3c3c797f1d1b46c79d6476
luciferldy <2460572865@qq.com>
2016-10-09 21:20:56 +0800

    use google service location and PlacePicker

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -30,6 +30,26 @@ import java.util.Date;
 import java.util.Locale;
 
 public class Utility {
+
+    public static float DEFAULT_LATLONG = 0f;
+
+    public static boolean isLocationLatLonAvailable(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.contains(context.getString(R.string.pref_location_latitude))
+                && prefs.contains(context.getString(R.string.pref_location_longitude));
+    }
+
+    public static float getLocationLatitude(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getFloat(context.getString(R.string.pref_location_latitude), DEFAULT_LATLONG);
+    }
+
+    public static float getLocationLongitude(Context context) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+        return pref.getFloat(context.getString(R.string.pref_location_longitude), DEFAULT_LATLONG);
+    }
+
+
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key),
